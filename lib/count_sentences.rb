c0c93397e4090083c22e9ab1407b830b0cdbc9f1
M Banks <montensia1@outlook.com>
2019-05-22 20:52:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    unless self.end_with?(".")
+    return false
+  else
+    return true
+  end
   end
 
   def question?
-
+      unless self.end_with?("?")
+    return false
+  else
+    return true
+  end
   end
 
   def exclamation?
-
+    unless self.end_with?("!")
+    return false
+  else
+    return true
+  end
   end
 
   def count_sentences
-
+    sentences_array = self.split(/\?|\.|\!|\!!/)
+    
+    i = 0
+    while i < sentences_array.length
+    binding.pry
+      i = i + 1
+    end
+    i
   end
 end
